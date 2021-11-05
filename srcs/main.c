0c19d426da291d87e5d815a91fadd17b2446e619
Ivette Kael <ikael@si-a3.kzn.21-school.ru>
2021-11-05 22:36:45 +0300

    main.c norm fix

--- srcs/main.c
+++ srcs/main.c
@@ -6,13 +6,13 @@
 /*   By: ikael <ikael@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/17 11:09:13 by ikael             #+#    #+#             */
-/*   Updated: 2021/11/05 02:08:56 by ikael            ###   ########.fr       */
+/*   Updated: 2021/11/05 22:35:47 by ikael            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d.h"
 
-static void player_set(t_data *data)
+static void	player_set(t_data *data)
 {
 	data->player.posx = data->player.posx * SIZE + SIZE / 2;
 	data->player.posy = data->player.posy * SIZE + SIZE / 2;
@@ -46,7 +46,7 @@ static void	game_int(t_data *data)
 	data->map.map = NULL;
 	data->delta_a = FOW / W_WIDTH;
 	data->h_fow = FOW / 2;
-	data->focus = (W_WIDTH/2)*tan(dtr(data->h_fow));
+	data->focus = (W_WIDTH / 2) * tan(dtr(data->h_fow));
 }
 
 static int	usage(void)
@@ -71,39 +71,13 @@ int	main(int argc, char **argv)
 	if (argc != 2)
 		return (usage());
 	game_int(&data);
-
 	if (get_map_identifiers(&data, argv[1]) == FAIL)
 		return (print_error("invalid identifiers", argv[1]));
-#ifdef DEBUG
-	printf("f-color: %X\n", data.map.f_color);
-	printf("c-color: %X\n", data.map.c_color);
-	printf("no_texture: %p\n", data.textures.north.img);
-	printf("so_texture: %p\n", data.textures.south.img);
-	printf("ea_texture: %p\n", data.textures.east.img);
-	printf("we_texture: %p\n", data.textures.west.img);
-#endif
-
 	if (get_map(&data, argv[1]) == FAIL)
 		return (print_error("invalid map", argv[1]));
-
-#ifdef DEBUG
-	char **tmp;
-	int	i = -1;
-	tmp = data.map.map;
-	printf("%10s\n", "MAP");
-	if (tmp) {
-		printf("width: %d    height: %d\n", data.map.width,
-			data.map.height);
-		while (tmp[++i])
-			printf(GREEN"|%s|\n"RESET, tmp[i]);
-		printf("posx: %f\nposy: %f\npl_direction: %c\n",
-			data.player.posx, data.player.posy, data.player.direct_view);
-	}
-#endif
-
 	player_set(&data);
-	mlx_hook(data.win, 2, 1L<<0, key_hook_press, &data);
-	mlx_hook(data.win, 3, 1L<<1, key_hook_release, &data);
+	mlx_hook(data.win, 2, 1L << 0, key_hook_press, &data);
+	mlx_hook(data.win, 3, 1L << 1, key_hook_release, &data);
 	mlx_hook(data.win, 17, 0, close_window, NULL);
 	mlx_loop_hook(data.mlx, render, &data);
 	mlx_loop(data.mlx);
