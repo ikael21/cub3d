68a5c2ba69d27c2dd612920f541a6a5c7409a6a1
Ivette Kael <ikael@si-b1.kzn.21-school.ru>
2021-11-06 08:37:42 +0300

    CUB3D
    Final version

--- srcs/graphic/select_text.c
+++ srcs/graphic/select_text.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   select_text.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: lvallie <marvin@42.fr>                     +#+  +:+       +#+        */
+/*   By: ikael <ikael@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/05 20:10:50 by lvallie           #+#    #+#             */
-/*   Updated: 2021/11/05 20:10:52 by lvallie          ###   ########.fr       */
+/*   Updated: 2021/11/05 22:52:55 by ikael            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,17 +17,17 @@ void	select_text(t_data *data, char *direct)
 	if ((int)(data->ray.prevy / SIZE) == (int)(data->ray.y / SIZE))
 	{
 		if ((int)(data->ray.prevx / SIZE) < (int)(data->ray.x / SIZE))
-			data->ray.texture = data->textures.west;
-		else
 			data->ray.texture = data->textures.east;
+		else
+			data->ray.texture = data->textures.west;
 		*direct = 'v';
 	}
 	else if ((int)(data->ray.prevx / SIZE) == (int)(data->ray.x / SIZE))
 	{
 		if ((int)(data->ray.prevy / SIZE) < (int)(data->ray.y / SIZE))
-			data->ray.texture = data->textures.north;
-		else
 			data->ray.texture = data->textures.south;
+		else
+			data->ray.texture = data->textures.north;
 		*direct = 'h';
 	}
 }
